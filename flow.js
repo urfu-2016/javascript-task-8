73e43714bd27e2fc7190b223a40b47e2d3eec835
Alex Solovyev <Alex Solovyev>
2016-12-01 12:06:06 +0500

    Back to backup

--- flow.js
+++ flow.js
@@ -41,36 +41,12 @@ exports.serial = function (operations, callback) {
  * @param {Function} callback
  */
 exports.map = function (items, operation, callback) {
-    function resultedMap(resultValues) {
-        return resultValues;
-    }
+    // function resultedMap(resultValues) {
+    //     return resultValues;
+    // }
 
-    baseMap(items, operation, callback, resultedMap);
-};
+    // baseMap(items, operation, callback, resultedMap);
 
-/**
- * Параллельная фильтрация элементов
- * @param {Array} items – элементы для фильтрация
- * @param {Function} operation – функция фильтрации элементов
- * @param {Function} callback
- */
-exports.filter = function (items, operation, callback) {
-    function resultedMap(resultValues, inputItems) {
-        return resultValues
-            .reduce(function (filteredItems, value, index) {
-                if (value) {
-                    filteredItems.push(inputItems[index]);
-                }
-
-                return filteredItems;
-            }, []);
-    }
-
-    baseMap(items, operation, callback, resultedMap);
-};
-
-
-function baseMap(items, operation, callback, resultedMap) {
     var result = {
         values: [],
         passedItemsCount: 0,
@@ -80,7 +56,7 @@ function baseMap(items, operation, callback, resultedMap) {
     if (items.length === 0) {
         callback(null, items);
     } else {
-        for (var i = 0; i < items.length && !result.errorOccurred; i++) {
+        for (var i = 0; i < items.length; i++) {
             operation(items[i], operationCallback.bind(null, i));
         }
     }
@@ -99,12 +75,112 @@ function baseMap(items, operation, callback, resultedMap) {
             result.passedItemsCount++;
 
             if (result.passedItemsCount === items.length) {
-                var resultForCallback = resultedMap(result.values, items);
-                callback(null, resultForCallback);
+                var mappedArray = result.values
+                    .filter(function (element) {
+                        return element;
+                    });
+
+                callback(null, mappedArray);
             }
         }
     }
-}
+};
+
+/**
+ * Параллельная фильтрация элементов
+ * @param {Array} items – элементы для фильтрация
+ * @param {Function} operation – функция фильтрации элементов
+ * @param {Function} callback
+ */
+exports.filter = function (items, operation, callback) {
+    // function resultedMap(resultValues, inputItems) {
+    //     return resultValues
+    //         .reduce(function (filteredItems, value, index) {
+    //             if (value) {
+    //                 filteredItems.push(inputItems[index]);
+    //             }
+
+    //             return filteredItems;
+    //         }, []);
+    // }
+
+    // baseMap(items, operation, callback, resultedMap);
+
+    var result = {
+        values: [],
+        passedItemsCount: 0,
+        errorOccurred: false
+    };
+
+    if (items.length === 0) {
+        callback(null, items);
+    } else {
+        for (var i = 0; i < items.length && !result.errorOccurred; i++) {
+            operation(items[i], operationCallback.bind(null, items[i], i));
+        }
+    }
+
+    function operationCallback(item, index, error, data) {
+        if (!data) {
+            data = error;
+            error = undefined;
+        }
+
+        if (error && !result.errorOccurred) {
+            callback(error, data);
+            result.errorOccurred = true;
+        } else {
+            result.values[index] = data ? item : data;
+            result.passedItemsCount++;
+
+            if (result.passedItemsCount === items.length) {
+                result.values = result.values
+                    .filter(function (element) {
+                        return element;
+                    });
+
+                callback(null, result.values);
+            }
+        }
+    }
+};
+
+
+// function baseMap(items, operation, callback, resultedMap) {
+//     var result = {
+//         values: [],
+//         passedItemsCount: 0,
+//         errorOccurred: false
+//     };
+
+//     if (items.length === 0) {
+//         callback(null, items);
+//     } else {
+//         for (var i = 0; i < items.length && !result.errorOccurred; i++) {
+//             operation(items[i], operationCallback.bind(null, i));
+//         }
+//     }
+
+//     function operationCallback(index, error, data) {
+//         if (!data) {
+//             data = error;
+//             error = undefined;
+//         }
+
+//         if (error && !result.errorOccurred) {
+//             callback(error, data);
+//             result.errorOccurred = true;
+//         } else {
+//             result.values[index] = data;
+//             result.passedItemsCount++;
+
+//             if (result.passedItemsCount === items.length) {
+//                 var resultForCallback = resultedMap(result.values, items);
+//                 callback(null, resultForCallback);
+//             }
+//         }
+//     }
+// }
 
 /**
  * Асинхронизация функций
